9c7cacab81895587d6f0231be4cb498356c6db11
danielevans-999 <danielevans.karani@gmail.comgit>
2019-10-27 14:18:21 +0300

    Adds a file adds the db to the dictionary parse to the shell

--- manage.py
+++ manage.py
@@ -3,16 +3,17 @@ from flask_script import Manager,Server
 from app.models import User,Post,Comment
 from flask_migrate import Migrate,MigrateCommand
 
+#creating app instance
 app = create_app('development')
 
 manage = Manager(app)
-migrate = (app,db)
+migrate = Migrate(app,db)
 manage.add_command('db',MigrateCommand)
 manage.add_command('server',Server)
 
 @manage.shell
 def make_shell_context():
-    return dict(app=app, User=User, Post=Post, Comment=Comment)
+    return dict(app = app, db = db , User = User, Post = Post, Comment = Comment)
 
 if __name__ == '__main__':
     manage.run()
