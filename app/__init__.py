91fe56efe090e95f42a63c53277603bb8a79c51c
danielevans-999 <danielevans.karani@gmail.comgit>
2019-10-27 15:17:05 +0300

    Initializes bootstrap to the application

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from config import config_options
 from flask_sqlalchemy import SQLAlchemy
+from flask_bootstrap import Bootstrap
 
 db =SQLAlchemy()
+bootstrap = Bootstrap()
 
 def create_app(config_name):
     
@@ -15,6 +17,7 @@ def create_app(config_name):
     # Initializing flask extensions
     
     db.init_app(app)
+    bootstrap.init_app(app)
     
     return app
     
