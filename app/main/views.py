1b194dbcdf9c2467795e146293d69375b5b2eb59
danielevans-999 <danielevans.karani@gmail.comgit>
2019-10-28 15:32:55 +0300

    Adds a  route that takes one to the form template to add a blog

--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,11 @@
 from . import main
-from flask import render_template
+from flask import render_template,redirect,url_for,request
 from ..requests import get_random_quote
 from ..models import User,Post,Comment
+from flask_login import login_required,current_user
+from ..import db
+from .forms import BlogForm
+
 
 @main.route('/')
 def index():
@@ -15,4 +19,21 @@ def blogs():
     
     return render_template('blogs.html',posts=posts)
     
+@main.route('/blogs/new', methods=["GET","POST"])
+@login_required
+def new_blog():
+    '''
+    view function that returns the blog form
+    '''
+    form = BlogForm()
+    if form.validate_on_submit():
+        post = form.post.data
+        title = form.title.data
+        
+        new_post = Post(title=title,body=post,user=current_user)
+        new_post.save_post()
+        return redirect(url_for('.blogs'))
+    
+    title='blogs'
+    return render_template('new_blog.html',title=title,blog_form=form)
     
