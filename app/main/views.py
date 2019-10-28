a89997af445c70886fab091ec6a711685ea89993
danielevans-999 <danielevans.karani@gmail.comgit>
2019-10-28 08:22:07 +0300

    Adds the view function thatwill return the blogs template

--- app/main/views.py
+++ app/main/views.py
@@ -1,9 +1,18 @@
 from . import main
 from flask import render_template
+from ..requests import get_random_quote
+from ..models import User,Post,Comment
 
 @main.route('/')
 def index():
+    quote = get_random_quote()
     
-    return render_template('index.html')
+    return render_template('index.html',quote=quote)
+
+@main.route('/blogs')
+def blogs():
+    posts = Post.query.all()
+    
+    return render_template('blogs.html',posts=posts)
     
     
