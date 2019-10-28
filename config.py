48b90a2cfe618275d4e96d212d2c43189fb8f7f8
danielevans-999 <danielevans.karani@gmail.comgit>
2019-10-28 03:26:44 +0300

    Adds  configuration for the quote base url

--- config.py
+++ config.py
@@ -5,7 +5,8 @@ class Config:
     General configuration parent class
     '''
     
-    
+    QUOTE_BASE_URL = 'http://quotes.stormconsultancy.co.uk/random.json'
+    SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_TRACK_MODIFICATIONS = False 
     
     
