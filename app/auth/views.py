523369bcaab12a8c01e401c85aec3526e7b3b3ce
danielevans-999 <danielevans.karani@gmail.comgit>
2019-10-28 09:51:43 +0300

    Adds a view function that will be responsible for the registration route

--- app/auth/views.py
+++ app/auth/views.py
@@ -10,7 +10,7 @@ from .. import db
 def register ():
     form = RegistrationForm()
     if form.validate_on_submit():
-        user = User(email=form.email.data, password=form.password.data,username=username.data)
+        user = User(email=form.email.data, password=form.password.data,username=form.username.data)
     
         db.session.add(user)
         db.session.commit()
